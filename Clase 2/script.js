27d43959b0c32e7a377840187e5cfe714e53c517
Francisco Pagnutti <pagnuttifrancisco@gmail.com>
2021-11-09 17:53:58 -0300

    ciclos para entregable 3

--- Clase 2/script.js
+++ Clase 2/script.js
@@ -1,12 +1,28 @@
-let nombre = prompt("Ingrese su nombre");
+let nombre = prompt("Por favor, ingrese su nombre.");
 
 if (nombre === ""){
-	prompt("Por favor, ingrese su nombre");
+	for (let i = 3; i > 0; i--){
+		nombre = prompt ("Ingrese su nombre. Le quedan " + i + " intentos");
+	}
 }else{
-	let color = prompt("Muy bien " + nombre + "! Adivine en que color estoy pensando... Ingreselo todo en minuscula");
-	if (color === "azul"){
-		console.log("Perfecto, ese era mi color");
-	}else{
-		console.log("Incorrecto! Mi color, era el azul.");
+	while (nombre !== ""){
+		let color = prompt("Muy bien " + nombre + "! Adivine en que color estoy pensando... Ingreselo todo en minuscula");
+		if (color === "azul"){
+			console.log("Perfecto, ese era mi color");
+		}else{
+			console.log("Incorrecto! Mi color, era el azul.");
+		}
+		nombre = prompt("Ingrese su nombre");
 	}
 }
+
+
+
+
+	
+	
+
+
+
+
+
