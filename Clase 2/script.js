2cb7dfd39bf1d0e056125979cf90092e336dd24b
Francisco Pagnutti <pagnuttifrancisco@gmail.com>
2021-11-11 14:20:40 -0300

    clase 4

--- Clase 2/script.js
+++ Clase 2/script.js
@@ -1,21 +1,43 @@
-let nombre = prompt("Por favor, ingrese su nombre.");
+let montoPrestamo = parseInt(prompt("Ingrese el monto de prestamo que desea obtener"));
+let cantidadCuotas = parseInt(prompt("Ingrese la cantidad de cuotas en las que desea financiar"));
+let intereses = 0;
+let sumaIntereses = 0;
+let cuotaTotal = 0;
+let interesCuota = 0;
 
-if (nombre === "", nombre !== "ESC"){
-	for (let i = 3; i > 0; i--){
-		nombre = prompt ("Ingrese su nombre. Le quedan " + i + " intentos");
-	}
-}else{
-	while (nombre !== ""){
-		let color = prompt("Muy bien " + nombre + "! Adivine en que color estoy pensando... Ingreselo todo en minuscula");
-		if (color === "azul"){
-			console.log("Perfecto, ese era mi color");
-		}else{
-			console.log("Incorrecto! Mi color, era el azul.");
-		}
-		nombre = prompt("Ingrese su nombre");
-	}
+function financiacion(){
+	intereses = 3 * cantidadCuotas;
+	sumaIntereses = montoPrestamo + ((montoPrestamo * intereses) / 100);
+}
+function cuotas(){
+	cuotaTotal = sumaIntereses / cantidadCuotas;
+	interesCuota = ((montoPrestamo * intereses) / 100) / cantidadCuotas;
+}
+function mostrar (){
+	console.log("Su prestamo tendra " + intereses + "% de interes. Dando asi un total de interes de $" + interesCuota + " por cuota.");
+	console.log("El valor de cada cuota sera de $" + cuotaTotal);
 }
 
+financiacion (montoPrestamo, cantidadCuotas);
+cuotas();
+mostrar();
+
+
+/*
+let montoPrestamo = parseInt(prompt("Ingrese el monto de prestamo que desea obtener"))
+let cantidadCuotas = parseInt(prompt("Ingrese la cantidad de cuotas en las que desea financiar"))
+
+function financiacion(){
+	const intereses = 3 * cantidadCuotas;
+	const sumaIntereses = montoPrestamo + ((montoPrestamo * intereses) / 100);
+	const cuotaTotal = sumaIntereses / cantidadCuotas;
+	const interesCuota = ((montoPrestamo * intereses) / 100) / cantidadCuotas;
+	console.log("Su prestamo tendra " + intereses + "% de interes. Dando asi un total de interes de $" + interesCuota + " por cuota.");
+	console.log("El valor de cada cuota sera de $" + cuotaTotal);
+}
+
+financiacion (montoPrestamo, cantidadCuotas);
+*/
 
 
 
