bf1633821e7385e7937abfb4ae3d703e76409f68
Francisco Pagnutti <pagnuttifrancisco@gmail.com>
2021-11-16 17:07:02 -0300

    objetvos. proyecto venta cripto

--- Clase 2/script.js
+++ Clase 2/script.js
@@ -1,48 +1,43 @@
-let montoPrestamo = parseInt(prompt("Ingrese el monto de prestamo que desea obtener"));
-let cantidadCuotas = parseInt(prompt("Ingrese la cantidad de cuotas en las que desea financiar"));
-let intereses = 0;
-let sumaIntereses = 0;
-let cuotaTotal = 0;
-let interesCuota = 0;
+let bnb = 630;
+let stock = 500000;
+let dolar = 195;
+let usdt = dolar;
+let paseDolar;
+let paseBnb;
+let cantidad = parseInt(prompt("Ingrese en pesos($) la cantidad que desea comprar"));
 
-function financiacion(){
-	intereses = 3 * cantidadCuotas;
-	sumaIntereses = montoPrestamo + ((montoPrestamo * intereses) / 100);
+class conversion{
+	constructor (cantidad, stock){
+	this.cantidad = parseFloat(cantidad);
+	this.stock = stock;
+	}
+	conversionDolar(){
+		paseDolar = cantidad / dolar; 
+	}
+	conversionBnb(){
+		paseBnb = paseDolar / bnb;
+	}
+	cantidadBnb (){
+		console.log("Usted comprara una suma de "+ paseDolar +" dolares en BNB. Equivalentes a " + paseBnb + "BNB");
+	}
 }
-function cuotas(){
-	cuotaTotal = sumaIntereses / cantidadCuotas;
-	interesCuota = ((montoPrestamo * intereses) / 100) / cantidadCuotas;
-}
-function mostrar (){
-	console.log("Su prestamo tendra " + intereses + "% de interes. Dando asi un total de interes de $" + interesCuota + " por cuota.");
-	console.log("El valor de cada cuota sera de $" + cuotaTotal);
-}
-
-financiacion (montoPrestamo, cantidadCuotas);
-cuotas();
-mostrar();
-
-
-/*
-let montoPrestamo = parseInt(prompt("Ingrese el monto de prestamo que desea obtener"))
-let cantidadCuotas = parseInt(prompt("Ingrese la cantidad de cuotas en las que desea financiar"))
-
-function financiacion(){
-	const intereses = 3 * cantidadCuotas;
-	const sumaIntereses = montoPrestamo + ((montoPrestamo * intereses) / 100);
-	const cuotaTotal = sumaIntereses / cantidadCuotas;
-	const interesCuota = ((montoPrestamo * intereses) / 100) / cantidadCuotas;
-	console.log("Su prestamo tendra " + intereses + "% de interes. Dando asi un total de interes de $" + interesCuota + " por cuota.");
-	console.log("El valor de cada cuota sera de $" + cuotaTotal);
-}
-
-financiacion (montoPrestamo, cantidadCuotas);
-*/
-
-
-
-	
-	
+if (cantidad > 0){
+	const compra = new conversion (cantidad, stock);
+	compra.conversionDolar();
+	compra.conversionBnb();
+	if (stock >= paseBnb){
+		compra.cantidadBnb();
+		let continuar = prompt("Desea continuar? Escriba si o no, segun corresponda.");
+		if (continuar === "si") {
+			console.log("Operacion exitosa. Ha realizado una compra de "+paseBnb+"BNB");
+			stock = stock - paseBnb;  
+		}else{
+			console.log("Operacion cancelada.");
+		}
+	}else {
+		console.log("La cantidad que usted desea comprar ("+paseBnb+") es superior a lo que tenemos disponible en este momento. Nuestra disponibilidad es de " + stock + " BNB");
+	}
+}	
 
 
 
