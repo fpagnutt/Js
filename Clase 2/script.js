312a023cda393a8e99f419028ea9ef12cfe194ff
Francisco Pagnutti <pagnuttifrancisco@gmail.com>
2021-11-18 16:43:31 -0300

    primer entrega proyecto final

--- Clase 2/script.js
+++ Clase 2/script.js
@@ -4,9 +4,11 @@ let dolar = 195;
 let usdt = dolar;
 let paseDolar;
 let paseBnb;
-let cantidad = parseInt(prompt("Ingrese en pesos($) la cantidad que desea comprar"));
+let datosComprador = [];
+let cantidad;
+let compra;
 
-class conversion{
+class Conversion{
 	constructor (cantidad, stock){
 	this.cantidad = parseFloat(cantidad);
 	this.stock = stock;
@@ -18,26 +20,48 @@ class conversion{
 		paseBnb = paseDolar / bnb;
 	}
 	cantidadBnb (){
-		console.log("Usted comprara una suma de "+ paseDolar +" dolares en BNB. Equivalentes a " + paseBnb + "BNB");
+		console.log("Usted comprara una suma de "+ paseDolar +" dolares en BNB. Equivalentes a " + paseBnb + " BNB");
 	}
 }
-if (cantidad > 0){
-	const compra = new conversion (cantidad, stock);
-	compra.conversionDolar();
-	compra.conversionBnb();
+
+class Compradores{
+	constructor(nombre, apellido, edad){
+		this.nombre = nombre.toUpperCase();
+		this.apellido = apellido.toUpperCase();
+		this.edad = edad.toString();
+	}
+}
+
+let nombre = prompt("Por favor, ingrese su nombre");
+let apellido = prompt ("Por favor, ingrese su apellido");
+let edad = prompt("Por ultimo, ingrese su edad");
+if (edad >= 18){
+	let comprador = new Compradores(nombre, apellido, edad);
+	datosComprador.push(comprador); 
+	cantidad = parseInt(prompt("Ingrese en pesos($) la cantidad que desea comprar"));
+	if (cantidad > 0){
+		compra = new Conversion(cantidad, stock);
+		compra.conversionDolar();
+		compra.conversionBnb();
+	}
 	if (stock >= paseBnb){
 		compra.cantidadBnb();
 		let continuar = prompt("Desea continuar? Escriba si o no, segun corresponda.");
 		if (continuar === "si") {
-			console.log("Operacion exitosa. Ha realizado una compra de "+paseBnb+"BNB");
-			stock = stock - paseBnb;  
+			console.log("Operacion exitosa. Ha realizado una compra de "+paseBnb+" BNB");
+			stock = stock - paseBnb;
 		}else{
 			console.log("Operacion cancelada.");
 		}
 	}else {
 		console.log("La cantidad que usted desea comprar ("+paseBnb+") es superior a lo que tenemos disponible en este momento. Nuestra disponibilidad es de " + stock + " BNB");
 	}
-}	
+}else{
+	console.log("Disculpe, este tipo de transaccion requiere que sea mayor de edad");
+}
+
+
+	
 
 
 
